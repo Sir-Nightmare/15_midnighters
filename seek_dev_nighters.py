750e98dbf87988fa20d89639ac6bd7f885e4d6c8
Sir Nightmare <ivantochilkin@gmail.com>
2016-10-11 13:03:39 +0300

    the script was done

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,43 @@
+import requests
+from pytz import timezone, utc
+from datetime import datetime, time
+
+API_URL = 'https://devman.org/api/challenges/solution_attempts/'
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    page = 1
+    number_of_pages = 1
+    while page <= number_of_pages:
+        devman_response = requests.get(API_URL, params={'page': page})
+        page_info = devman_response.json()
+        number_of_pages = int(page_info['number_of_pages'])
+        for record in page_info['records']:
+            if record['timestamp']:
+                yield record
+        page += 1
+
+
+def local_time(time_stamp, zone):
+    local_time_zone = timezone(zone)
+    utc_date_time = datetime.fromtimestamp(float(time_stamp)).replace(tzinfo=utc)
+    local_date_time = utc_date_time.astimezone(local_time_zone)
+    return local_date_time.time()
 
 def get_midnighters():
-    pass
+    set_of_midnighters = set()
+    for record in load_attempts():
+        user_time = local_time(record['timestamp'], record['timezone'])
+        if time(0,0,0) < user_time < time(6,0,0):
+            set_of_midnighters.add(record['username'])
+    return sorted(set_of_midnighters)
+
+
+def print_the_list(list_to_print):
+    print('The list of midnighters:')
+    for username in list_to_print:
+        print(username)
 
 if __name__ == '__main__':
-  pass
+    midnighters = get_midnighters()
+    print_the_list(midnighters)
